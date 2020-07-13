198d876cba42eb7ee11cf9d7e9fcaaf92d5aaedc
Alvaro Martin <62333486+alvaromartin47-rgb@users.noreply.github.com>
2020-07-13 15:30:02 -0300

    Implementacion de heap_sort y heap_crear_arr con sus pruebas (#3)
    * Implementacion de heap_sort y heap_crear_arr con sus pruebas
    
    * Correcciones de revision, falta AUMENTAR. Separo declaraciones en aux.c.
    
    * Agrego aux.h
    
    * Quito declaraciones de los .c
    
    * Agrego constantes AUMENTAR y DISMINUIR

--- heap/heap.c
+++ heap/heap.c
@@ -1,27 +1,42 @@
-#include "heap.h"
+#include "aux.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-/**************************DECLARACIONES****************************/
-
-/*******************************************************************/
-
-/* *****************************************************************
- *                          ESTRUCTURAS                            *
- * *****************************************************************/
-
-/* Principal */
-
-/*******************************************************************/
+#define CAP_INICIAL 20
+#define AUMENTAR * 2
+#define DISMINUIR / 2
 
 /* *****************************************************************
  *                        PRIMITIVAS HEAP                          *
  * *****************************************************************/
 
-/*******************************************************************/
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
+    heap_t *heap = heap_crear(cmp);
+    if (!heap) return NULL;    
+    
+    for (size_t i=0; i < n; i++) {
+        if (!arreglo[i]) return NULL;
+        if (heap->cantidad == heap->capacidad) {
+            heap_redimensionar(heap, heap->capacidad AUMENTAR);
+        }
+        heap->datos[i] = arreglo[i];
+        heap->cantidad++;
+    }
 
-/* *****************************************************************
- *                      FUNCIONES AUXILIARES                       *
- * *****************************************************************/
+    heapify(heap->datos, (int)heap->cantidad, cmp);
+
+    return heap;
+}
+
+void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp) {
+    int _cant = (int)cant;
+    heapify(elementos, _cant, cmp);
+    for (int i=1; i < cant; i++) {
+        swap(elementos, 0, _cant - i);
+        downheap(elementos, _cant - i, cmp, 0);
+    }
+}
+
+/*******************************************************************/
--- heap/pruebas_alumno_heap.c
+++ heap/pruebas_alumno_heap.c
@@ -4,10 +4,40 @@
 #include <string.h>
 #include "testing.h"
 #include "heap.h"
+#include "hash.h"
+#include <time.h>
 
 /* ******************************************************************
  *                       FUNCIONES AUXILIARES                       *
  * *****************************************************************/
+
+int cmp_enteros(const void *str1, const void *str2) {
+    int num1 = atoi((char*)str1);
+    int num2 = atoi((char*)str2);
+    if (num1 > num2) return 1;
+    if (num1 < num2) return -1;
+    return 0;
+}
+
+void obt_aleatorios_y_guardar(hash_t *hash, char **claves, size_t largo) {
+    
+    srand((unsigned int)time(NULL));
+
+    for (size_t i=0; i < largo; i++) {
+        char *clave = malloc(sizeof(char) * 10);
+        size_t num = (size_t)rand()%(largo) + 1;
+        sprintf(clave, "%ld", num);
+        
+        while (hash_pertenece(hash, clave)) {
+            num = (size_t)rand()%(largo) + 1;
+            sprintf(clave, "%ld", num);
+        }
+
+        hash_guardar(hash, clave, &num);
+        claves[i] = clave;
+    }
+
+}
 
 /*******************************************************************/
 
@@ -15,6 +45,63 @@
  *                        PRUEBAS UNITARIAS                         *
  * *****************************************************************/
 
-void pruebas_heap_alumno();
+void prueba_heap_crear_arr(size_t largo) {
+    hash_t *hash = hash_crear(NULL);
+    char **claves = malloc(sizeof(char*) * largo);
+
+    obt_aleatorios_y_guardar(hash, claves, largo);
+    
+    heap_t *heap = heap_crear_arr(claves, largo, cmp_enteros);
+    //heap_sort(claves, largo, cmp_enteros);
+    
+    bool ok = true;
+
+    while (!heap_esta_vacio(heap)) {
+        char *desencolado = heap_desencolar(heap);
+        
+        if (heap_cantidad(heap) > 1) {
+            ok = cmp(desencolado, (char*)heap_ver_max(heap)) > -1;
+            if (!ok) break;
+        }
+
+        ok = hash_pertenece(hash, desencolado);
+        if (!ok) break;
+    }
+
+    print_test("Prueba heap crear a partir de arreglo", ok);
+
+    hash_destruir(hash);
+    heap_destruir(heap, NULL);
+    for (size_t i=0; i < largo; i++) free(claves[i]);
+    free(claves);
+}
+
+void prueba_heap_sort(size_t largo) {
+    hash_t *hash = hash_crear(NULL);
+    char **claves = malloc(sizeof(char*) * largo);
+
+    obt_aleatorios_y_guardar(hash, claves, largo);
+    
+    heap_sort(claves, largo, cmp_enteros);
+    
+    bool ok = true;
+
+    for (size_t i=0; i < largo; i++) {
+        if (i < largo - 1) ok = cmp_enteros(claves[i], claves[i + 1]) < 1;  
+        if (!ok) break;
+    }
+
+    print_test("Prueba ordenar con heapsort a partir de arreglo", ok);
+
+    hash_destruir(hash);
+    for (size_t i=0; i < largo; i++) free(claves[i]);
+    free(claves);
+}
+
+
+void pruebas_heap_alumno() {
+    prueba_heap_crear_arr(1000);
+    prueba_heap_sort(1000);
+}
 
 /*******************************************************************/
